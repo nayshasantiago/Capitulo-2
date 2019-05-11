9a224972ab41f154c01bf38619b77dc4f6a31dd6
Naysha <nayshasolange833@gmail.com>
2019-05-11 07:32:02 -0500

    Create Ejercicio18.java

---
+++ Ejercicio18.java
@@ -0,0 +1,19 @@
+/* Escribe un programa que muestre la siguiente tabla:
+  a b pow (a, b)
+  1   2    1.0
+  2   3    8.0
+  3   4    81.0
+  4   5    1024.0
+  5   6   15625.0
+ **/
+package capitulo2;
+class Ejercicio18 {
+    	public static void main(String[] args) {
+	System.out.println("a     b     pow(a, b)");
+	System.out.println("1     2     " + Math.pow(1, 2));
+        System.out.println("2     3     " + Math.pow(2, 3));
+	System.out.println("3     4     " + Math.pow(3, 4));
+        System.out.println("4     5     " + Math.pow(4, 5));
+	System.out.println("5     6     " + Math.pow(5, 6));
+    }
+}
